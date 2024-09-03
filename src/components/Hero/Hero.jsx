b9f869e8eab830a09a03f86c0ac3f5440e0590d8
MarkusLYZ <markoos203@gmail.com>
2024-09-03 11:14:58 +0800

    asset-update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Markus</h1>
+        <h1 className={styles.title}>Hi, I'm Yunzhi</h1>
         <p className={styles.description}>
           I am a full-stack mobile developer with 1 year of experience. Reach
           out if you'd like to learn more!
