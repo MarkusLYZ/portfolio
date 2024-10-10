eee81017c2390ec8c3f28ced582313f5dec9e889
MarkusLYZ <markoos203@gmail.com>
2024-10-10 23:41:42 +0800

    skillsUpdate

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,6 +8,7 @@ export const Contact = () => {
       <div className={styles.text}>
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
+        <p>+65 92427987</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
